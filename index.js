c04b8e8d284c46d48594ca13a99a40ea8b9dfa66
Paavo Juutilainen <paavojuutilainen@kamk.fi>
2024-07-02 14:16:08 +0300

    3.1 - 3.6 valmis

--- index.js
+++ index.js
@@ -1,1 +1,108 @@
-console.log("Moi")
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+      {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": "1"
+      },
+      {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": "2"
+      },
+      {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": "3"
+      },
+      {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423666",
+        "id": "4"
+      }
+]
+
+  
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${Date()}</p>
+        `)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+  })
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+  })
+
+ //https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range 
+const generateId = () => {
+    const min = 0 //included
+    const max = 1000000 //included
+    const id = Math.floor(Math.random() * (max-min+1)) + min // min=0 now!
+    return String(id)
+}
+
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    //console.log(body.name)
+
+    const name = request.body.name
+    const existingName = persons.find(person => person.name === name)
+    
+    //console.log(existingName)
+
+    if (existingName) {
+        return response.status(400).json({
+            error: 'name already in phonebook, use unique name',
+        })
+    }
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing',
+        })
+    }
+
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing',
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number || false,
+        id: generateId(),
+    }
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+  
+const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
