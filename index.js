95117493669054315d6d0fbd516556229ac1f374
Paavo Juutilainen <paavojuutilainen@kamk.fi>
2024-07-11 10:57:32 +0300

    3.9 valmis, muutoksia myös osan 2 repossa

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('data', function (req) {
     //const method = JSON.stringify(req.method)
