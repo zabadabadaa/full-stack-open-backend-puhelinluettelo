af5c0b498941f131935293c8215115e05a6c1163
Paavo Juutilainen <paavojuutilainen@kamk.fi>
2024-07-11 16:50:16 +0300

    3.12 valmis

---
+++ mongo.js
@@ -0,0 +1,47 @@
+//tehtävä 3.12
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://zabadabadaa:${password}@cluster0.aikrlqj.mongodb.net/Puhelinluettelo?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', noteSchema)
+
+if (process.argv.length === 5) {
+    const name = process.argv[3]
+    const number = process.argv[4]
+    const person = new Person({
+        name: name,
+        number: number,
+      })
+
+    person.save().then(result => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+    })
+}
+
+if (process.argv.length === 3) {
+    console.log("phonebook:")
+    Person.find({}).then(result => {
+        result.forEach(person => {
+        console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
+    })
+}
+
