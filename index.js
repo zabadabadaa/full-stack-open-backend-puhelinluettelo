de2f450fcd356cfd74fe5caa298017a3c1ef7b07
Paavo Juutilainen <paavojuutilainen@kamk.fi>
2024-07-15 15:31:54 +0300

    3.19 valmis

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ const errorHandler = (error, request, response, next) => {
   
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message})
     }
   
     next(error)
@@ -92,7 +94,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
   })
 
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     //console.log(body.name)
 
@@ -118,7 +120,8 @@ app.post('/api/persons', (request, response) => {
     person.save().then(result => {
         response.json(result)
         console.log('person saved!')
-      })  
+      })
+      .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
